f3a8bfdb0f2415820b10090550a5848d112fd6f6
Mirko Bay <mibay@htwg-konstanz.de>
2018-05-02 22:47:43 +0200

    refactored configuration file

--- src/main/java/de/htwg/swqs/catalog/CatalogConfiguration.java
+++ src/main/java/de/htwg/swqs/catalog/CatalogConfiguration.java
@@ -1,23 +1,15 @@
 package de.htwg.swqs.catalog;
 
-import de.htwg.swqs.catalog.repository.CatalogRepository;
-import de.htwg.swqs.catalog.service.CatalogService;
-import de.htwg.swqs.catalog.service.CatalogServiceImpl;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootConfiguration
+@Configuration
 @ComponentScan("de.htwg.swqs.catalog")
 @EnableJpaRepositories("de.htwg.swqs.catalog.repository")
 @EntityScan("de.htwg.swqs.catalog.model")
 public class CatalogConfiguration {
 
-  @Bean
-  public CatalogService catalogService(CatalogRepository catalogRepository) {
-    return new CatalogServiceImpl(catalogRepository);
-  }
-
+  // the beans are already defined by the class annotations
 }
